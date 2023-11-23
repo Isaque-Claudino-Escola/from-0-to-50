7ac27f0b649c5cb2004101b0162a49e50aa31b89
Isaque-Claudino-Escola <86322136+Isaque-Claudino-Escola@users.noreply.github.com>
2023-11-23 22:37:15 +0000

    first commit

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) {
+        int min = 0;
+        int max = 50;
+
+        while (min <= max) {
+            System.out.println(min);
+            min++;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Trabalho Java
+> Usando o laço de repetição While
+
+
+Escreva um programa em Java que: 
+- imprima na tela os números de 0 a 50.
+- Compile e poste o resultado aqui.
